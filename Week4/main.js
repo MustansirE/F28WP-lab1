83d0ce910af29257614106167ac41679e3e15077
Mustansir Eranpurwala <mte2000@hw.ac.uk>
2023-11-03 10:13:24 +0400

    Break added

--- Week4/main.js
+++ Week4/main.js
@@ -2,18 +2,18 @@ var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://MustansirE.github.io/F28WP-lab1/Week4/cities1.json');
-ourRequest.onload = function() {
-var ourData = JSON.parse(ourRequest.responseText);
-renderHTML(ourData);
-btn.classList.add("hide-me");
-};
-ourRequest.send();
+    ourRequest.open('GET', 'https://MustansirE.github.io/F28WP-lab1/Week4/cities1.json');
+    ourRequest.onload = function() {
+    var ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData);
+    btn.classList.add("hide-me");
+    };
+    ourRequest.send();
 });
 function renderHTML(data){
     var htmlString = "";
     for (i=0; i<data.length; i++){
-    htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + "\nWhere you can enjoy indoor places like: " ;
+    htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + "<br>\nWhere you can enjoy indoor places like: " ;
 
         for (ii = 0; ii < data[i].places.indoor.length; ii++) {
         // Loop through the indoor places of the current city.
