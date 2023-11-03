ec40347886418b84951540da80dbbf2a74312dfe
Mustansir Eranpurwala <mte2000@hw.ac.uk>
2023-11-03 09:36:11 +0400

    Initial commit

---
+++ Week4/main.js
@@ -0,0 +1,5 @@
+ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
+ourRequest.onload = function() {
+console.log(ourRequest.responseText);
+};
+ourRequest.send();
